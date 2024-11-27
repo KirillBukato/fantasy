212cb029bac273d3ec0585b9f078565b00eb7e2a
kirsiv40 <ttvgch@gmail.com>
2024-11-27 10:52:29 +0300

    obtaining user from token

--- src/main/java/by/bsu/fantasy/controller/UserController.java
+++ src/main/java/by/bsu/fantasy/controller/UserController.java
@@ -2,20 +2,35 @@ package by.bsu.fantasy.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import by.bsu.fantasy.model.User;
 import by.bsu.fantasy.service.UserService;
+import by.bsu.fantasy.util.JwtTokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final JwtTokenRepository jwtTokenRepository;
 
     @GetMapping("/users/all")
     private List<User> getAll() {
         return userService.getUSers();
+    }
+
+    @GetMapping("/users/fromtoken")
+    private ResponseEntity<User> getUserFromToken(HttpServletRequest request) {
+        try {
+            User user = jwtTokenRepository.getUserFromRequest(request);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
+        }
     }
 }
